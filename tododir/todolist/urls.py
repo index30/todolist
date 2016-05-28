7e152801deba587fb2c6c2f257424e234f788a86
ranthy <realthingshakes25@gmail.com>
2016-05-29 00:36:25 +0900

    Make only TODO list. Decide layout etc...
    TOP,list,make,deleteを実装
    topに何を記載するかは考えていないので、何か思いつけば追加する方針
    todolistとして必要最低限な機能は備えたので、まずはログイン機能などを追加する
    それが終わってから細かな部分の修正を加えていこうと思う

--- tododir/todolist/urls.py
+++ tododir/todolist/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^(?P<task_id>[0-9]+)/$',views.task_content,name='detail'),
+    url(r'^$',views.top,name='top'),
+    url(r'^detail/$',views.index,name='index'),
+    url(r'^detail/(?P<task_id>[0-9]+)/$',views.task_content,name='detail'),
+    url(r'^detail/(?P<task_id>[0-9]+)/delete$',views.delete_task,name='delete'),
     url(r'^make/$',views.make_task,name='make'),
 ]
--- tododir/todolist/views.py
+++ tododir/todolist/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
+
+import datetime
+
+def top(request):
+    return render(request, 'todolist/top.html')
 
 def index(request):
     task_list = Task.objects.all()
@@ -18,4 +22,24 @@ def task_content(request, task_id):
     return render(request, 'todolist/detail.html',task_context)
 
 def make_task(request):
-    return render(request, 'todolist/make.html')
+    try:
+        id=4
+        m_title = request.POST['title']
+        m_text = request.POST['text']
+        m_done = False
+        m_created_at = datetime.datetime.now()
+        m_updated_at = datetime.datetime.now()
+        m_finished_at = request.POST['finish']
+    except (KeyError,Task.DoesNotExist):
+        return render(request,'todolist/make.html')
+    else:
+        #Task t
+        #t.m_title = 
+        Task(title=m_title,text=m_text,done=m_done,created_at=m_created_at,updated_at=m_updated_at,finished_at=m_finished_at).save()
+    return redirect('index')
+
+def delete_task(request,task_id):
+    d_task = get_object_or_404(Task,pk=task_id)
+    print(d_task)
+    d_task.delete()
+    return redirect('index')
