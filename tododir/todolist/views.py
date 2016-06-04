54c1e5965a329dc9e2e7cd8db2a03fb891df33c4
ranthy <realthingshakes25@gmail.com>
2016-06-05 02:07:15 +0900

    Almost finish TODO list.
    ナビゲーションバーにおけるログイン・ログアウトの表示・非表示
    タスクとユーザーの結びつきを修正(上手くユーザごとに反映されるようにした)
    細かい部分(パスワードの秘匿化や、reCAPTCHAの追加、デザインの変更等)はまた後で

--- tododir/todolist/views.py
+++ tododir/todolist/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect,get_object_or_404,render_to_response
 from django.core.context_processors import csrf
-from django.contrib.auth import authenticate,login,logout
+from django.template import RequestContext
+from django.contrib.auth import authenticate,login,logout,models
 from django.contrib.auth.decorators import login_required
 
 from .models import Task
-#from .models import User
 from django.contrib.auth.models import User
 
 import datetime
@@ -13,11 +13,11 @@ from .forms import NewUserCreationForm as UserCreationForm
 from django.views.decorators.http import require_GET
 
 def top(request):
-    return render(request, 'todolist/top.html')
+    return render_to_response('todolist/top.html',RequestContext(request,{}))
 
 @login_required
 def index(request):
-    task_list = Task.objects.all()
+    task_list = Task.objects.filter(user=request.user)
     context = {
         'task_list':task_list
     }
@@ -41,12 +41,12 @@ def make_task(request):
         m_created_at = datetime.datetime.now()
         m_updated_at = datetime.datetime.now()
         m_finished_at = request.POST['finish']
+        m_user = request.user
+        
     except (KeyError,Task.DoesNotExist):
         return render(request,'todolist/make.html')
     else:
-        #Task t
-        #t.m_title = 
-        Task(title=m_title,text=m_text,done=m_done,created_at=m_created_at,updated_at=m_updated_at,finished_at=m_finished_at).save()
+        Task(title=m_title,text=m_text,done=m_done,created_at=m_created_at,updated_at=m_updated_at,finished_at=m_finished_at,user=m_user).save()
     return redirect('index')
 
 @login_required
@@ -55,14 +55,6 @@ def delete_task(request,task_id):
     print(d_task)
     d_task.delete()
     return redirect('index')
-
-#@require_GET
-#def to_register(request):
-#    form = UserCreationForm()
-#    c = {'form': form}
-#    c.update(csrf(request))
-#    print('toregi')
-#    return render_to_response('todolist/make_user.html', c)
 
 def register(request):
     try:
@@ -74,27 +66,6 @@ def register(request):
     else:
         User.objects.create_user(u_name,u_email,u_pass).save()
     return redirect('top')
-
-#
-#def signup(request):
-#    try:
-#        u_name=request.POST['user_name']
-#        u_pass=request.POST['password']
-#        user = authenticate(username=u_name, password=u_pass)
-#    except (KeyError,User.DoesNotExist):
-#        return render(request,'todolist/signup.html')
-#    else:
-#        if user is not None:
-#            if user.is_active:
-#                login(request, user)
-#                return redirect('top')
-#            else:
-#                form_errors = True
-#            return render(request,'todolist/signup.html')
-#        else:
-#            form_invalid=True
-#        return render(request,'todolist/signup.html')
-
 
 def signout(request):
     logout(request)
